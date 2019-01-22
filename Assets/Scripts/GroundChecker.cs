e3ceef9ee05085094fc1f825dae42fc3c1ff43e0
Emrik Lind <emrik.lind@elev.ga.lbs.se>
2019-01-22 14:16:51 +0100

    La till Groundchecker script

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public int isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground") isGrounded++;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground") isGrounded--;
+    }
+}
